3bdd24f383eca01f6e83cbd850a57ca5e4549198
DESKTOP-VOMF6EQ\Remch <remch183@outlook.com>
2019-04-28 00:54:44 +0800

    Updates

---
+++ precache-manifest.670f7d18473940f182ff8e5d2b91494b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "58bc2934123bdf07a7d7747a2865b785",
+    "url": "/index.html"
+  },
+  {
+    "revision": "419f7aca992e396cbf21",
+    "url": "/static/css/main.882825aa.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "419f7aca992e396cbf21",
+    "url": "/static/js/main.bc5ba69e.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
